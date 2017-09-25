a0c24109ac96b82fdd9fae8d41288507c6309979
bKiraly <kiraly.bence.1025@gmail.com>
2017-09-25 09:48:01 +0200

    Fixed tabs

--- JAVA/Programming2/Ex3_Visibility.java
+++ JAVA/Programming2/Ex3_Visibility.java
@@ -1,19 +1,19 @@
 class Ex3_Visibility{
                 public static void main(String args[]){
                                Admin admin = new Admin("sajt", "sajt", "sajt@sajt.com", "sajt");
-                               System.out.println(admin.login("sajt","sajt"));                                                  //>true                #Here we show that we can access the inherited method of user from admin
-                               User user = admin.addUser("sonka", "sonka@sonka.com", "sonka", "sajt");        //Here we show we can still use admin methods to create a user
-                               System.out.println(user.login("sonka", "sonka"));                                           //>true
+                               System.out.println(admin.login("sajt","sajt"));				//>true		#Here we show that we can access the inherited method of user from admin
+                               User user = admin.addUser("sonka", "sonka@sonka.com", "sonka", "sajt");	//Here we show we can still use admin methods to create a user
+                               System.out.println(user.login("sonka", "sonka"));			//>true
                                
                                //Success case: Password changed successfully
-                               System.out.println(user.setPassword("sonka", "sonka2", "sonka2"));     //>true                #The password was changed successfully
-                               System.out.println(user.login("sonka", "sonka"));                                            //>false #The old credentials no longer work
-                               System.out.println(user.login("sonka", "sonka2"));                                         //>true                #The new credentials work
+                               System.out.println(user.setPassword("sonka", "sonka2", "sonka2"));	//>true		#The password was changed successfully
+                               System.out.println(user.login("sonka", "sonka"));			//>false	#The old credentials no longer work
+                               System.out.println(user.login("sonka", "sonka2"));			//>true		#The new credentials work
                                
                                //Failure case: Password unchanged
-                               System.out.println(user.setPassword("something", "sonka", "sonka")); //>false #Update failed, wrong password provided
-                               System.out.println(user.login("sonka", "sonka2"));                                         //>true                #Old credentials still work
-                               System.out.println(user.login("sonka", "sonka"));                                           //>false #New credentials don not work
+                               System.out.println(user.setPassword("something", "sonka", "sonka"));	//>false	#Update failed, wrong password provided
+                               System.out.println(user.login("sonka", "sonka2"));			//>true		#Old credentials still work
+                               System.out.println(user.login("sonka", "sonka"));			//>false	#New credentials don not work
                 }
                 public static class User{
                                 String username;
