ca3faa5ad622eb333a5bcf46761455eac2031f8b
bKiraly <kiraly.bence.1025@gmail.com>
2017-10-08 23:24:57 +0200

    Update ExA_WordSorter.java

--- JAVA/Programming2/ExA_WordSorter.java
+++ JAVA/Programming2/ExA_WordSorter.java
@@ -1,3 +1,5 @@
+package JavaCourse;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -5,32 +7,13 @@ import javafx.scene.layout.*;
 import javafx.scene.text.*;
 import javafx.stage.Stage;
 
-public class ExA_WordSorter extends Application{
-	//Helper method to find the column the word belongs to
-	public static int findColumnOfWord(String word) throws IllegalArgumentException{
-		//Local variable to hold the first charcter
-		char firstChar;
-		
-		//Exception handling
-		if(word.length() == 0) throw new IllegalArgumentException();
-		
-		//firstChar is the first character of the word
-		firstChar = word.charAt(0);
-		
-		//If it is a capital letter, the column will be its distance from 'A'
-		if(firstChar >= 'A' && firstChar <= 'Z') return firstChar-'A';
-		//If it is a lowecase letter, the column will be its distance from 'a'
-		else if(firstChar >= 'a' && firstChar <= 'z') return firstChar-'a';
-		//Otherwise we'll put it in the last column
-		else return 26;
-	}
-	
+public class EXA_WordSorter extends Application{
 	//Globals
 	VBox[] wordContainer;
 	TextField tf;
 	
 	public void start(Stage s) {
-		//Global intialization and local declaration
+		//Global initialization and local declaration
 		BorderPane applicationContainer = new BorderPane();
 		HBox tableContainer;
 		HBox textBoxContainer;
@@ -39,14 +22,14 @@ public class ExA_WordSorter extends Application{
 		Text tmpT;
 		
 		//Create the columns
-		for(int i = 0; i < wordContainer.length; i++) {
-			tmpT = new Text(""+(char)(i==26?'?':'A'+i));	//If we are in the last column, we write '?'. Otherwise write the i-th letter
+		for(char i = 'A'; i <= 'Z'+1; i++) {
+			tmpT = new Text(""+(i==('Z'+1)?'?':i));			//If we are in the last column, we write '?'. Otherwise write the i-th letter
 			tmpT.setFont(new Font("Helvetica", 20));		//Make the font large for the headings
-			wordContainer[i] = new VBox(tmpT);
+			wordContainer[i-'A'] = new VBox(tmpT);
 		}
 		tableContainer = new HBox(wordContainer);
 		applicationContainer.setCenter(tableContainer);
-		
+
 		//Creating the text field
 		tf = new TextField();
 		addButton = new Button("Add");
@@ -54,8 +37,13 @@ public class ExA_WordSorter extends Application{
 		addButton.setOnAction(a->{
 			String text = this.tf.getText();
 			try {
-				//The helper function tells us which column to put the word in.
-				this.wordContainer[WordSorter.findColumnOfWord(text)].getChildren().add(new Text(text));
+				if(text.length() == 0) throw new IllegalArgumentException();
+				//We take the first character in the string
+				char firstChar = text.toUpperCase().charAt(0);
+				//Calculate array index from the first character
+				int arrayIndex = (firstChar-'A'<='Z'&firstChar>='A')?firstChar-'A':26;
+				//Add the text to the respective container
+				this.wordContainer[arrayIndex].getChildren().add(new Text(text));
 			} catch (IllegalArgumentException e){
 				//No bother, the input is empty, we don't need to do anything
 			}
