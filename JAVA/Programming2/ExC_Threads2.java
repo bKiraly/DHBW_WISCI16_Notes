a6635bfca58a7e2c045f268bd0c546156595b38d
bKiraly <kiraly.bence.1025@gmail.com>
2017-11-18 14:38:54 +0100

    Create ExC_Threads2.java

---
+++ JAVA/Programming2/ExC_Threads2.java
@@ -0,0 +1,21 @@
+package JavaCourse;
+
+public class Thread2 {
+	public static class MyThread extends Thread{
+		private int stepSize;
+		private String prefix;
+		public MyThread(int stepSize, String prefix){
+			this.stepSize = stepSize;
+			this.prefix = prefix;
+		}
+		@Override
+		public void run() {
+			for(int i = 0; i < 10; i += this.stepSize)
+				System.out.println(prefix+" | "+i);
+		}
+	}
+	public static void main(String args[]) {
+		new MyThread(2,"A").start();
+		new MyThread(3,"B").start();
+	}
+}
