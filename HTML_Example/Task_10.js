80202a5b69fafc95efb23906c50ebaa947f41261
bKiraly <kiraly.bence.1025@gmail.com>
2018-01-25 12:42:53 +0100

    Create Task_10.js

---
+++ HTML_Example/Task_10.js
@@ -0,0 +1,12 @@
+var firstH1;
+
+window.addEventListener("load", _=>{
+	firstH1 = document.getTagName("h1")[0];
+	window.addEventListener("mouseMove", (X,Y)=>{
+		firstH1.innerText = "Mouse is at: "+X+"/"+Y;
+	});
+	window.addEventListener("keydown", _=>{
+		documnet.style.backgroundColor="#0f0";
+	});
+	document.style.backgroundColor="#fff";
+})
