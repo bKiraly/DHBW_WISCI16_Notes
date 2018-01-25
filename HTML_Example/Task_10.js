1f73053e8e7ca21eb93dd02c958823a143fd20cf
bKiraly <kiraly.bence.1025@gmail.com>
2018-01-25 12:55:46 +0100

    Update Task_10.js

--- HTML_Example/Task_10.js
+++ HTML_Example/Task_10.js
@@ -1,12 +1,12 @@
 var firstH1;
 
 window.addEventListener("load", _=>{
-	firstH1 = document.getElementByTagName("h1")[0];
-	window.addEventListener("mouseMove", (X,Y)=>{
-		firstH1.innerText = "Mouse is at: "+X+"/"+Y;
+	firstH1 = document.getElementById("sajt");
+	window.addEventListener("mousemove", (e)=>{
+		firstH1.innerText = "Mouse is at: "+e.x+"/"+e.y;
 	});
 	window.addEventListener("keydown", _=>{
-		documnet.style.backgroundColor="#0f0";
+		document.body.style.backgroundColor="#0f0";
 	});
-	document.style.backgroundColor="#fff";
+	document.body.style.backgroundColor="#fff";
 })
